c835e921708f7743eb909305b301ac8db3264ec4
gabrielbar <gabrieel.barreto@gmail.com>
2025-07-17 21:11:03 -0300

    ajuste corrigido

--- src/Redux/store.ts
+++ src/Redux/store.ts
@@ -1,4 +1,8 @@
 import { applyMiddleware, createStore } from "redux";
 import { rootReducer } from "./root-reduce";
 import logger from "redux-logger";
-export const store = createStore(rootReducer, applyMiddleware(logger));
+export const store = createStore(
+  rootReducer,
+  undefined,
+  applyMiddleware(logger)
+);
